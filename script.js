2493fb58162088cd129a30fda6a3093b011d5d0e
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2022-05-13 00:02:31 +0530

    Adds playing and scoring

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let game_started = false
+let your_score = opponent_score = 0
+
 function clearPlayGround() {
     let buttons = document.getElementById("play_field").getElementsByTagName("button")
     for (var i = 0 ; i < buttons.length; i++) {
@@ -27,6 +30,52 @@ function toggle() {
 }
 
 const mark = button_id => {
-    document.getElementById(button_id).innerText = 'X';
-    document.getElementById("inputs").getElementsByTagName("p")[0].innerText = "Opponent's Turn"
+    if (document.getElementById(button_id).innerText == "") {
+        document.getElementById(button_id).innerText = 'X'
+        if (check_win('X') == true) {
+            return
+        }
+        document.getElementById("inputs").getElementsByTagName("p")[0].innerText = "Opponent's Turn"
+        opponent_mark()
+    } 
 }
+
+function opponent_mark() {
+    let button_id = Math.floor(Math.random()*9 + 1)
+    while (document.getElementById(button_id).innerText != "") {
+        button_id = Math.floor(Math.random()*9 + 1)
+    }
+    // console.log(button_id)
+    document.getElementById(button_id).innerText = 'O'
+    if (check_win('O') == true) {
+        return
+    }
+    document.getElementById("inputs").getElementsByTagName("p")[0].innerText = "Your Turn"
+}
+
+function check_win(symbol) {
+    let buttons = document.getElementById("play_field").getElementsByTagName("button")
+    if (((buttons[0].innerText == symbol) && (buttons[1].innerText == symbol) && (buttons[2].innerText == symbol)) ||
+        ((buttons[3].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[5].innerText == symbol)) ||
+        ((buttons[6].innerText == symbol) && (buttons[7].innerText == symbol) && (buttons[8].innerText == symbol)) ||
+        ((buttons[0].innerText == symbol) && (buttons[3].innerText == symbol) && (buttons[6].innerText == symbol)) ||
+        ((buttons[1].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[7].innerText == symbol)) ||
+        ((buttons[2].innerText == symbol) && (buttons[5].innerText == symbol) && (buttons[8].innerText == symbol)) ||
+        ((buttons[0].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[8].innerText == symbol)) ||
+        ((buttons[2].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[6].innerText == symbol))) {
+            // Increament Score
+            if (symbol == "X") {
+                your_score++;
+                document.getElementById("resulttext_1").innerText = your_score;
+            } else {
+                opponent_score++;
+                document.getElementById("resulttext_2").innerText = opponent_score;
+            }
+            toggle()
+            return true;
+        }
+    return false;
+}
+
+
+
