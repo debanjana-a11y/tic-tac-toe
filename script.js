d88385449a318b7577aab560546c651b35e43539
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2022-09-11 00:05:14 +0530

    Adds primary commits for game play, replay, score reveal etc

--- script.js
+++ script.js
@@ -1,83 +1,133 @@
-let game_started = false
-let your_score = opponent_score = 0
-
-function clearPlayGround() {
-    let buttons = document.getElementById("play_field").getElementsByTagName("button")
-    for (var i = 0 ; i < buttons.length; i++) {
-        buttons[i].innerText = ""
-        buttons[i].disabled = true
+const gameBoard = (() => {
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let winboard = ['012', '345', '678', '036', '147', '258', '048', '246'];
+    
+    function player(number, name) {
+        this.number = number;
+        this.name = name;
+        return {number, name};
     }
-}
 
-function enablePlayGround() {
-    let buttons = document.getElementById("play_field").getElementsByTagName("button")
-    for (var i = 0 ; i < buttons.length; i++) {
-        buttons[i].disabled = false
-    }
-}
+    const player_1 = player(1, 'X');
+    const player_2 = player(2, 'O');
 
-function toggle() {
-    let text = document.getElementById("play").innerText;
-    if (text == "Start") {
-        enablePlayGround()
-        document.getElementById("play").innerText = "Reset"
-    } else {
-        clearPlayGround()
-        document.getElementById("play").innerText = "Start"
-    }
-}
+    let playing_now = undefined;
 
-const mark = button_id => {
-    if (document.getElementById(button_id).innerText == "") {
-        document.getElementById(button_id).innerText = 'X'
-        if (check_win('X') == true) {
-            return
+    changePlayer(player_1);
+
+    function changePlayer(player) {
+        const turn_info = document.getElementsByClassName('turn_info')[0];
+        const element = document.createElement('span');
+        if (playing_now !== undefined) {
+            turn_info.removeChild(turn_info.lastChild);
         }
-        opponent_mark()
-    } 
-}
+        
+        playing_now = player;
+        element.textContent = playing_now.number;
+        turn_info.append(element);
+    }
 
-function opponent_mark() {
-    let button_id = Math.floor(Math.random()*9 + 1)
-    let count = 20;
-    while (document.getElementById(button_id).innerText != "" && count > 0) {
-        button_id = Math.floor(Math.random()*9 + 1)
-        count--;
+    function changeResult(text) {
+        const result = document.getElementsByClassName('result')[0];
+        result.innerText = text;
     }
-    if (count == 0) {
-        alert ("Game Over!!")
-        return
-    }
-    // console.log(button_id)
-    document.getElementById(button_id).innerText = 'O'
-    if (check_win('O') == true) {
-        return
-    }
-}
 
-function check_win(symbol) {
-    let buttons = document.getElementById("play_field").getElementsByTagName("button")
-    if (((buttons[0].innerText == symbol) && (buttons[1].innerText == symbol) && (buttons[2].innerText == symbol)) ||
-        ((buttons[3].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[5].innerText == symbol)) ||
-        ((buttons[6].innerText == symbol) && (buttons[7].innerText == symbol) && (buttons[8].innerText == symbol)) ||
-        ((buttons[0].innerText == symbol) && (buttons[3].innerText == symbol) && (buttons[6].innerText == symbol)) ||
-        ((buttons[1].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[7].innerText == symbol)) ||
-        ((buttons[2].innerText == symbol) && (buttons[5].innerText == symbol) && (buttons[8].innerText == symbol)) ||
-        ((buttons[0].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[8].innerText == symbol)) ||
-        ((buttons[2].innerText == symbol) && (buttons[4].innerText == symbol) && (buttons[6].innerText == symbol))) {
-            // Increament Score
-            if (symbol == "X") {
-                your_score++;
-                document.getElementById("resulttext_1").innerText = your_score;
-            } else {
-                opponent_score++;
-                document.getElementById("resulttext_2").innerText = opponent_score;
+    function checkWinner() {
+        let player_1_choice = "";
+        let player_2_choice = "";
+        for(let idx in board) {
+            if (board[idx] == 'X') {
+                player_1_choice = player_1_choice + idx;
             }
-            toggle()
-            return true;
+            if (board[idx] == 'O') {
+                player_2_choice = player_2_choice + idx;
+            }
         }
-    return false;
-}
+        
+        for (let e of winboard) {
+            let player_1_win = 0;
+            let player_2_win = 0;
+      
+            for(let i = 0; i < e.length; i++) {
+                let ch = e.charAt(i);
+                if (playing_now === player_2) {
+                    if (player_1_choice.includes(ch) === true) {
+                        player_1_win++;
+                        if (player_1_win === 3) {
+                            return player_1;
+                        }
+                    } else {
+                        break;
+                    }
+                } else {
+                    if (player_2_choice.includes(ch) === true) {
+                        player_2_win++;
+                        if (player_2_win === 3) {
+                            return player_2;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    changeResult('Game Starting ...');
+
+    (function addListener() {
+        const boxlist = document.getElementsByClassName('box');
+        for(const box of boxlist) {
+            box.addEventListener('click', (e) => {
+                let idx = e.currentTarget.id;
+                if (playing_now.number == 1) {
+                    e.currentTarget.textContent = 'X';
+                    board[idx] = 'X';
+                    changePlayer(player_2);
+                } else {
+                    e.currentTarget.textContent = 'O';
+                    e.currentTarget.style.color = 'green';
+                    board[idx] = 'O';
+                    changePlayer(player_1);
+                }
+                changeResult('Game Ongoing ...');
+                const winner = checkWinner();
+                if (winner !== null) {
+                    changeResult('Our winner is ' + winner.number);
+                }
+            });
+        };
+    })();
+
+    function emptyBoard() {
+        board.clear();
+    }
+
+})();
+
+const displayController = (() => {
+    (function addListener() {
+        let play_btn = document.getElementsByClassName('btn_play')[0];
+        play_btn.addEventListener("click", (e) => {
+            const game_arena = document.getElementsByClassName('game_arena')[0];
+            game_arena.style.display = 'flex';
+
+            const btn_replay = document.getElementsByClassName('btn_replay')[0];
+            btn_replay.style.display = 'flex';
+            e.currentTarget.style.display = 'none';
+        });
+
+
+        const btn_replay = document.getElementsByClassName('btn_replay')[0];
+        btn_replay.addEventListener("click", (e) => {
+            const boxlist = document.getElementsByClassName('box');
+            for(const box of boxlist) {
+               box.textContent = '';
+            };
+        });
+    })();
+})();
 
 
 
